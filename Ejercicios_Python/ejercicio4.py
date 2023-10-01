b58da34d8aeff04f699fe23a4bdd10e1973663b5
AdriFdezz <afergar613@g.educaand.es>
2023-10-01 19:04:31 +0200

    Ejercicio 4

---
+++ Ejercicios_Python/ejercicio4.py
@@ -0,0 +1,8 @@
+def Conversor_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+if __name__ == "__main__":
+    celsius = int(input("Ingresa la temperatura en grados Celsius: "))
+    fahrenheit = Conversor_fahrenheit(celsius)
+    print(str(celsius) + " grados Celsius equivalen a " , int(fahrenheit) , " grados Fahrenheit.")
--- Test_ejercicios/test_ejercicios.py
+++ Test_ejercicios/test_ejercicios.py
@@ -1,6 +1,7 @@
 from Ejercicios_Python.ejercicio1 import bienvenida
 from Ejercicios_Python.ejercicio2 import importe_total
 from Ejercicios_Python.ejercicio3 import operaciones
+from Ejercicios_Python.ejercicio4 import Conversor_fahrenheit
 
 def test_bienvenida():
     resultado = bienvenida("Adri")
@@ -17,3 +18,7 @@ def test_operaciones():
     resultado = operaciones()
     salida = "Operacion 1: 8.5\nOperacion 2: 8\nOperacion 3: 4.0\nOperacion 4: 11"
     assert resultado == salida
+
+def test_conversor_fahrenheit():
+    resultado = Conversor_fahrenheit(30)
+    assert resultado == 86
